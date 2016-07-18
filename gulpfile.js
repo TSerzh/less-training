47dfa56c220f5ceed996c780582231ed4f8ef660
TsSerg <s.lukumon@gmail.com>
2016-07-18 19:17:58 +0300

    Some changes - added html files to watch and ...

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'), // Подключаем Gulp
     browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('less', function(){ // Создаем таск "less"
-    return gulp.src('app/less/**/*.less') // Берем источник
+    return gulp.src('app/less/main.less') // Берем источник
         .pipe(less()) // Преобразуем Sass в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
@@ -21,5 +21,6 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('watch', ['browser-sync', 'less'], function() {
     gulp.watch('app/less/**/*.less', ['less']); // Наблюдение за less файлами
+    gulp.watch('*.html', browserSync.reload);
     // Наблюдение за другими типами файлов
 });
